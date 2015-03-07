4ebdb1cfef022b58fae15c062dcfef99aebd9fce
Arief Ridwan <aridwan05@gmail.com>
2015-03-07 14:40:00 +0700

    fixed

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,20 +1,20 @@
 
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-header">
+                        <h1 id="type">Selamat Datang di SI-MATASA</h1>
+                    </div>
+                </div>
+            </div>
+                                                <!-- Headings -->
+            <div class="jumbotron">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <div class="page-header">
-                            <h1 id="type">Selamat Datang di SI-MATASA</h1>
-                        </div>
+                    <div class="col-lg-10 col-lg-offset-1">
+                        <h3>Anda berhasil masuk ke halaman Tata Usaha</h3>
                     </div>
                 </div>
-                                                    <!-- Headings -->
-                <div class="jumbotron">
-                    <div class="row">
-                        <div class="col-lg-10 col-lg-offset-1">
-                            <h3>Anda berhasil masuk ke halaman Tata Usaha</h3>
-                        </div>
-                    </div>
-                </div>
+            </div>
         </div>
     </div>
