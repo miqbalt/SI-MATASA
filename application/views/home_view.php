1a60b74354da2b20d0c8223fec206d86eaecad1e
Dewi Maya Fitriana <maya.dmf@gmail.com>
2015-03-24 03:17:27 +0700

    =home

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="page-header">
-                        <h1 id="type">Selamat Datang di SI-MATASA</h1>
+                        <h1 id="type">Selamat Datang di SIAKAD-MATASA</h1>
                     </div>
                 </div>
             </div>
@@ -12,7 +12,7 @@
             <div class="jumbotron">
                 <div class="row">
                     <div class="col-lg-10 col-lg-offset-1">
-                        <h3>Anda berhasil masuk ke halaman Tata Usaha</h3>
+                        <h3>Login Berhasil !</h3>
                     </div>
                 </div>
             </div>
